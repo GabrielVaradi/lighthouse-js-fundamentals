c5954dab9979853621982e3dab45a824e66efe53
Gabriel Varadi <gabriel.varadi.hallope.lecoq@gmail.com>
2019-03-21 15:08:11 +0000

    add ingredients.js

---
+++ ingredients.js
@@ -0,0 +1,20 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+var contentsWhile = 0
+while (contentsWhile < ingredients.length) {
+  console.log(ingredients[contentsWhile]);
+  contentsWhile++;
+}
+
+
+for (var contents = 0 ; contents < ingredients.length ; contents++){
+  console.log(ingredients[contents])
+}
+
+
+
+for (var backwards = 7 ; backwards > 0 ; backwards--){
+  console.log(ingredients[backwards])
+}
+
+
